f3ca664e60da89e6ef15c4285173f29d8ba1ac3e
HP <adiibrahimov@gmail.com>
2023-02-17 19:55:04 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+Eagle eagle = new Eagle();
+Parrot parrot = new Parrot();
+Bee bee = new Bee();
+Helicopter helicopter = new Helicopter();
+Airplane airplane = new Airplane();
+Aerostat aerostat = new Aerostat();
+Helicopter [] hangar = {helicopter,aerostat,airplane};
+Eagle [] zoo ={eagle,parrot,bee};
+    }
+}
